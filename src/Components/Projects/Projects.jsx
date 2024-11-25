be47bf3834a820c89a4f71934cadb4c9fff496e1
Victoria 2451 <qscrev98@gmail.com>
2024-11-25 18:17:14 +1030

    Add Project Component
    - Add the Project Component
    - Add a CSS style sheet for it
    - Add some mock data for testing purpose.

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,5 +1,51 @@
+import Title from "../Title/Title";
+import "./Projects.css";
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <Title title="Projects" subTitle="" />
+      <div className="project">
+        <div className="project-header">
+          <h3 className="project-title">Machine-Demo-Using-VR</h3>
+          <div className="project-subtitle">
+            University of South Australia, Adelaide, South Australia
+          </div>
+        </div>
+        <div className="project-attri">
+          <div>Objective</div>
+          <p className="description">
+            Help Future Industry to demonstrate their latest in-line coating
+            techs to their potential customers (usually located
+            overseas/interstate)
+          </p>
+        </div>
+        <div className="project-attri">
+          <div>Tools</div>
+          <p className="description">Unity 3D, C#</p>
+        </div>
+        <div className="project-attri">
+          <div>Duties</div>
+          <p className="description">
+            Co-Developer in a dual developer group. I was in charge of writing
+            the animation scripts in C#. Those scripts run in the backend and
+            animate the 3D objects in the Application.
+          </p>
+        </div>
+        <div className="project-attri">
+          <div>Outcome</div>
+          <p className="description">
+            We developed an app that works on PC (with HTC VIVE), IOS and
+            Android
+          </p>
+        </div>
+        <div className="project-attri">
+          <div>Result</div>
+          <p className="description">Obtained a Distinction for the project</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
