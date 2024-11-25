d4594d3416db1b457b0bfe1b71b11da1ddd7986a
Victoria 2451 <qscrev98@gmail.com>
2024-11-25 15:05:28 +1030

    Refactor the Skills.jsx
    - Add a Category component
    - Dynamically generate the category with  the new react component

---
+++ src/Components/Category/Category.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+const Category = ({ title, items }) => {
+  return (
+    <div className="category">
+      <h2>{title}</h2>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+Category.propTypes = {
+  title: PropTypes.string.isRequired, // Ensures title is a required string
+  items: PropTypes.arrayOf(PropTypes.string).isRequired, // Ensures items is an array of strings
+};
+
+export default Category;
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,49 +1,34 @@
 import "./Skills.css";
 import data from "../../data/data.json";
-
+import Category from "../Category/Category";
 import Title from "../Title/Title";
 
 const Skills = () => {
+  const categories = [
+    {
+      title: "Programming Languages",
+      items: ["JavaScript", "HTML5", "Python", "CSS"],
+    },
+    {
+      title: "Frameworks",
+      items: ["React JS", "React Native", "Node JS", ".NET"],
+    },
+    {
+      title: "Development Software",
+      items: ["VS Code", "Visual Studio", "Sublime", "Eclipse"],
+    },
+    {
+      title: "Libraries",
+      items: ["Bootstrap", "D3.js", "Node.js", "Scikit-learn python"],
+    },
+  ];
   return (
     <div className="skills">
       <Title subTitle="" title="My Skills" />
       <div className="myskills">
-        <div className="category">
-          <h2>Programming Languages</h2>
-          <ul>
-            <li>JavaScript</li>
-            <li>HTML5</li>
-            <li>Python</li>
-            <li>CSS</li>
-          </ul>
-        </div>
-        <div className="category">
-          <h2>Frameworks</h2>
-          <ul>
-            <li>React JS</li>
-            <li>React Native</li>
-            <li>Node JS</li>
-            <li>.NET</li>
-          </ul>
-        </div>
-        <div className="category">
-          <h2>Development Software</h2>
-          <ul>
-            <li>VS Code</li>
-            <li>Visual Studio</li>
-            <li>Sublime</li>
-            <li>Eclipse</li>
-          </ul>
-        </div>
-        <div className="category">
-          <h2>Libraries</h2>
-          <ul>
-            <li>Bootstrap</li>
-            <li>D3.js</li>
-            <li>Node.js</li>
-            <li>Scikit-learn python</li>
-          </ul>
-        </div>
+        {categories.map((category, index) => (
+          <Category key={index} title={category.title} items={category.items} />
+        ))}
       </div>
     </div>
   );
