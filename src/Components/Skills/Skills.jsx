989ce5aa4dbdc2033873952d4a73bf163d44c510
Victoria 2451 <qscrev98@gmail.com>
2024-11-25 15:26:11 +1030

    Update data.json with skills data
    - Add skills data into data.json

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -4,29 +4,12 @@ import Category from "../Category/Category";
 import Title from "../Title/Title";
 
 const Skills = () => {
-  const categories = [
-    {
-      title: "Programming Languages",
-      items: ["JavaScript", "HTML5", "Python", "CSS"],
-    },
-    {
-      title: "Frameworks",
-      items: ["React JS", "React Native", "Node JS", ".NET"],
-    },
-    {
-      title: "Development Software",
-      items: ["VS Code", "Visual Studio", "Sublime", "Eclipse"],
-    },
-    {
-      title: "Libraries",
-      items: ["Bootstrap", "D3.js", "Node.js", "Scikit-learn python"],
-    },
-  ];
+  const categories = data.Skills;
   return (
     <div className="skills">
       <Title subTitle="" title="My Skills" />
       <div className="myskills">
-        {categories.map((category, index) => (
+        {data.Skills.categories.map((category, index) => (
           <Category key={index} title={category.title} items={category.items} />
         ))}
       </div>
