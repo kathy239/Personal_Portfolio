a458b2b94581b4852fc5277d9e1617fb234a6980
Victoria 2451 <qscrev98@gmail.com>
2024-11-28 09:10:18 +1030

    Add softskills to the Skills

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,37 +3,6 @@ import "./Projects.css";
 import Project from "./Project";
 import data from "../../data/data.json";
 const Projects = () => {
-  const projectData = [
-    {
-      title: "Machine-Demo-Using-VR",
-      subtitle: "University of South Australia, Adelaide, South Australia",
-      attributes: [
-        {
-          label: "Objective",
-          description:
-            "Help Future Industry to demonstrate their latest in-line coating techs to their potential customers (usually located overseas/interstate)",
-        },
-        {
-          label: "Tools",
-          description: "Unity 3D, C#",
-        },
-        {
-          label: "Duties",
-          description:
-            "Co-Developer in a dual developer group. I was in charge of writing the animation scripts in C#. Those scripts run in the backend and animate the 3D objects in the Application.",
-        },
-        {
-          label: "Outcome",
-          description:
-            "We developed an app that works on PC (with HTC VIVE), IOS and Android",
-        },
-        {
-          label: "Result",
-          description: "Obtained a Distinction for the project",
-        },
-      ],
-    },
-  ];
   return (
     <div className="projects">
       <Title title="Projects" subTitle="" />
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -4,13 +4,23 @@ import Category from "../Category/Category";
 import Title from "../Title/Title";
 
 const Skills = () => {
-  const categories = data.Skills;
+  const SoftSkills = data.SoftSkills || { desc: "No description available." };
+
   return (
     <div className="skills">
       <Title subTitle="" title="Technical Skills" />
       <div className="myskills">
         {data.Skills.map((category, index) => (
           <Category key={index} title={category.title} items={category.items} />
+        ))}
+      </div>
+      <Title subTitle="" title="Soft Skills" />
+      <div className="myskills">
+        {SoftSkills.map((data, index) => (
+          <div className="category" key={index}>
+            <h2>{data.skill}</h2>
+            <p>{data.desc}</p>
+          </div>
         ))}
       </div>
     </div>
